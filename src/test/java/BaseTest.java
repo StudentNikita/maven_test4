e31aa6922807e88a103f687878c6ac602cf82e3a
Avdonin Nikita <48263067+StudentNikita@users.noreply.github.com>
2019-03-24 15:49:59 +0300

    с чекбоксом недоработано

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,27 +1,31 @@
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
+import org.junit.*;
 
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.GeckoDriverInfo;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
-    public WebDriver driver;
+    public  static WebDriver driver;
 
-    @Before
-    public void setUp() throws Exception {
+    @BeforeClass
+    public static void setUp()  {
 
         System.out.println("Тест по селениум!");
 
@@ -42,14 +46,59 @@ public class BaseTest {
 
         driver.get(url);
 
+        clicByXpath("//ol/li/a[contains(text(), 'Страхование')]");
+        clicByXpath("//*[contains(text(), 'Выезжающим за рубеж')]");
+        scrollToObjectByXPath("//a[contains(text(), 'Рассчитать')]");
+        clicByXpath("//a[contains(text(), 'Рассчитать')]");
+
+        Wait<WebDriver> wait = new WebDriverWait(driver, 10, 1000);
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//div[contains(@class, 'page')]/span[@class='h1']")))); // ждем, когда появится нужный нам объект
+
+        compareText(By.xpath("//div[contains(@class, 'page')]/span[@class='h1']"), "Страхование выезжающих за рубеж");
+
+
+
+        scrollToObjectByXPath("//button/*[contains(@class, 'content-title')]");
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//button/*[contains(@class, 'content-title')]")))); // ждем, когда появится нужный нам объект
+        //scrollToObjectByXPath("//button/*[contains(@class, 'content-title')]"); // вейт и скрол менял местами (был крол потом вейт)
+
+        clicByXpath("//button/*[contains(@class, 'content-title')]");
+
+        fillForm(By.xpath("//*[contains(@class, 'actual-input')]"), "шенген");
+        scrollToObjectByXPath("//*[contains(@class, 'actual-input')]");
+        clicByXpath("//*[contains(@class, 'tt-menu tt')]");
+        clicByName("ArrivalCountryList");
+        clicByXpath("//*[contains(text(), 'Испания')]");
+        clicByName("ArrivalCountryList");
+
     }
 
-    @After
-    public void close() {
+    @AfterClass
+    public static void close() {
+        Wait<WebDriver> wait = new WebDriverWait(driver, 10, 1000);
+        scrollToObjectByXPath("//input[@data-test-name='IsProcessingPersonalDataToCalculate']");
+
+
+        if (!driver.findElement(By.xpath("//input[@data-test-name='IsProcessingPersonalDataToCalculate']")).isSelected())  // проверка на "галочку" у чекбокса, если галочка не стоит, то ставим
+        {
+            clicByXpath("//input[@data-test-name='IsProcessingPersonalDataToCalculate']");
+
+        }
+
+        clicByXpath("//div[contains(@class, 'valid')]/button[contains(text(), 'Рассчитать')]");
+
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//div[contains (text(),'Комфорт')]"))));
+        scrollToObjectByXPath("//h1[contains (text(),'Кальку')]");
+
+
+        compareText(By.xpath("//div/span[@data-bind]/span[contains(@class, 'text-bold')]"), "Многократные поездки в течение года");
+        compareText(By.xpath("//span/span/strong[@data-bind='text: Name']"), "Шенген");
+        compareText(By.xpath("//strong[@data-bind=\"text: LastName() + ' ' + FirstName()\"]"), "LERCHIK" + " PERCHIK");
+        compareText(By.xpath("//strong[@data-bind=\" text: BirthDay.repr('moscowRussianDate')\"]"), "06." + "08." + "1994");
         driver.quit();
     }
 
-    public void fillForm(By locator, String value) {
+    public static void fillForm(By locator, String value) {
         driver.findElement(locator).click();
         driver.findElement(locator).clear();
         try {
@@ -62,7 +111,7 @@ public class BaseTest {
 
     }
 
-    public static String todayPlusTwoWeeks() {
+    public  String todayPlusTwoWeeks() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
         Calendar instance = Calendar.getInstance();
         instance.setTime(new Date());
@@ -71,27 +120,33 @@ public class BaseTest {
         return newDate;
     }
 
-    public void clicByXpath(String xPath) {
+    public static void clicByXpath(String xPath) {
         driver.findElement(By.xpath(xPath)).click();
 
     }
 
-    public void clicByName(String name) {
+    public static void clicByName(String name) {
         driver.findElement(By.name(name)).click();
 
     }
 
-    public String getTextByXpath(String xPath) {
+    public static String getTextByXpath(String xPath) {
         return driver.findElement(By.xpath(xPath)).getText();
 
     }
 
-    public void scrollToObjectByXPath(String xPath) {
+    public static void scrollToObjectByXPath(String xPath) {
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", driver.findElement(By.xpath(xPath)));
     }
 
-    public void compareText(By locator, String expect) {
+    public static void compareText(By locator, String expect) {
        Assert.assertTrue("Искомого текста НЕТ: " + expect + ". Вместо него: " + driver.findElement(locator).getText(), driver.findElement(locator).getText().contains(expect));
         System.out.println("Искомый текст ЕСТЬ: " + expect);
     }
+
+
+
+
+
 }
+
--- src/test/java/RgsTest.java
+++ src/test/java/RgsTest.java
@@ -18,7 +18,7 @@ public class RgsTest extends BaseTest {
     @Parameterized.Parameters
         public static Collection<Object[]> data() {
             return Arrays.asList(new Object[][] {
-                    { "Misha", "Pysha","09111990", true }, { "PERCHIK", "LERCHIK","06081994", false }
+                    { "Misha", "Pysha","09111990", false }, { "IVAN", "PETROV","01011998", false} ,{ "PERCHIK", "LERCHIK","06081994", true}
             });
         }
 
@@ -34,70 +34,63 @@ public class RgsTest extends BaseTest {
         @Parameterized.Parameter(3) //планирует или нет активный отдых
         public  boolean rest;
 
+
+
     @Test
     public void rgs(){
 
-        clicByXpath("//ol/li/a[contains(text(), 'Страхование')]");
-        clicByXpath("//*[contains(text(), 'Выезжающим за рубеж')]");
-        scrollToObjectByXPath("//a[contains(text(), 'Рассчитать')]");
-        clicByXpath("//a[contains(text(), 'Рассчитать')]");
-
-        Wait<WebDriver> wait = new WebDriverWait(driver, 10, 1000);
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//div[contains(@class, 'page')]/span[@class='h1']")))); // ждем, когда появится нужный нам объект
-
-        compareText(By.xpath("//div[contains(@class, 'page')]/span[@class='h1']"), "Страхование выезжающих за рубеж");
 
 
-        scrollToObjectByXPath("//button/*[contains(@class, 'content-title')]");
 
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//button/*[contains(@class, 'content-title')]")))); // ждем, когда появится нужный нам объект
-
-
-        clicByXpath("//button/*[contains(@class, 'content-title')]");
-
-        fillForm(By.xpath("//*[contains(@class, 'actual-input')]"), "шенген");
-        clicByXpath("//*[contains(@class, 'tt-menu tt')]");
-        clicByName("ArrivalCountryList");
-        clicByXpath("//*[contains(text(), 'Испания')]");
-        clicByName("ArrivalCountryList");
         scrollToObjectByXPath("//*[contains(@class, 'clear')]/*[contains(@class, 'form-group margin')]/*[contains(@class, 'control-label')]");
         clicByXpath("//*[contains(@class, 'clear')]/*[contains(@class, 'form-group margin')]/*[contains(@class, 'control-label')]");
 
+
+        scrollToObjectByXPath("//*[contains(@class, 'clear')]/*[contains(@class, 'form-group margin')]/*[contains(@class, 'form-control')]");
         do fillForm(By.xpath("//*[contains(@class, 'clear')]/*[contains(@class, 'form-group margin')]/*[contains(@class, 'form-control')]"), todayPlusTwoWeeks());
         while (getTextByXpath("//span[contains(text(), 'Заполните')]").contains("Заполните"));
 
         clicByXpath("//*[contains(text(), '90 дней')]");
 
-        fillForm(By.xpath("//div[@data-fi-input-mode=\"combined\"]//div[@class=\"form-group\"]//input[@class=\"form-control\" != @disabled]"), firstName + " " + lastName);
+        do fillForm(By.xpath("//*[contains(@class, 'form-control')][contains(@data-bind, 'attr: _params.fullName.attr,')]"), firstName + " " + lastName);
+        while (getTextByXpath("//*[contains(@class, 'form-control')][contains(@data-bind, 'attr: _params.fullName.attr,')]").equals(firstName + " " + lastName));
 
-        fillForm(By.xpath("//input[@data-test-name='BirthDate']"), bDay);
+        do fillForm(By.xpath("//input[@data-test-name='BirthDate']"), bDay);
+        while (getTextByXpath("//input[@data-test-name='BirthDate']").equals(bDay));
+
+
+
         scrollToObjectByXPath("//*[contains(text(), 'активный отдых или спорт')]/ancestor::div[@class='calc-vzr-toggle-risk-group']//div[@class='toggle off toggle-rgs']");
 
 
-       if (rest == true) clicByXpath("//*[contains(text(), 'активный отдых или спорт')]/ancestor::div[@class='calc-vzr-toggle-risk-group']//div[@class='toggle off toggle-rgs']");
+           if (!driver.findElement(By.xpath("//*[contains(text(), 'активный отдых или спорт')]/ancestor::div[@class='calc-vzr-toggle-risk-group']//div[@class='toggle off toggle-rgs']")).isSelected()) {
+              clicByXpath("//*[contains(text(), 'активный отдых или спорт')]/ancestor::div[@class='calc-vzr-toggle-risk-group']//div[@class='toggle off toggle-rgs']");
+              clicByXpath("//div[contains(@class, 'toggle toggle-rgs')]");
+           }
 
 
 
-        scrollToObjectByXPath("//input[@data-test-name='IsProcessingPersonalDataToCalculate']");
 
 
-        if (!driver.findElement(By.xpath("//input[@data-test-name='IsProcessingPersonalDataToCalculate']")).isSelected())  // проверка на "галочку" у чекбокса, если галочка не стоит, то ставим
-        {
-            clicByXpath("//input[@data-test-name='IsProcessingPersonalDataToCalculate']");
-
-        }
-
-        clicByXpath("//div[contains(@class, 'valid')]/button[contains(text(), 'Рассчитать')]");
-
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//div[contains (text(),'Комфорт')]"))));
-        scrollToObjectByXPath("//h1[contains (text(),'Кальку')]");
 
 
-        compareText(By.xpath("//div/span[@data-bind]/span[contains(@class, 'text-bold')]"), "Многократные поездки в течение года");
-        compareText(By.xpath("//span/span/strong[@data-bind='text: Name']"), "Шенген");
-        compareText(By.xpath("//strong[@data-bind=\"text: LastName() + ' ' + FirstName()\"]"), "LERCHIK" + " PERCHIK");
-        compareText(By.xpath("//strong[@data-bind=\" text: BirthDay.repr('moscowRussianDate')\"]"), "06." + "08." + "1994");
-        
+
+
+
+
+
+
+
+
+
+
+
+        //if (rest == true) clicByXpath("//div[contains(@class, 'toggle toggle-rgs')]/div[contains(@class, 'toggle-group')]/label[contains(text(), 'нет') and contains(@class, 'toggle')]");
+       //if (rest == false) clicByXpath("//div[contains(@class, 'toggle toggle-rgs')]/div[contains(@class, 'toggle-group')]/label[contains(text(), 'да') and contains(@class, 'toggle')]");
+        //div[contains(@class, 'toggle toggle-rgs')]/div[contains(@class, 'toggle-group')]/label[contains(text(), 'нет') and contains(@class, 'toggle')]
+
+
+
     }
 
 }
